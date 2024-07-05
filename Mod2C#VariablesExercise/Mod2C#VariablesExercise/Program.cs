0501ac436415ee21a42f91a7f5ccf46fb3612313
PamelaNicks <pamelanicks@yahoo.com>
2024-07-05 00:32:28 -0500

    Mod2C#Variables

---
+++ Mod2C#VariablesExercise/Mod2C#VariablesExercise/Program.cs
@@ -0,0 +1,15 @@
+﻿// Declare and initialize variables
+string name = "Pamela";
+int age = 47;
+char middleInitial = 'J';
+bool isOver18 = true;
+double currentTemp = 98.6;
+decimal currentPrice = 712.76m;
+
+// Writing the variables to the console
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Over 18: {isOver18}");
+Console.WriteLine($"Current Temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
